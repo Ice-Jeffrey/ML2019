4ef1801c5db549c3e0702692dd5d41aa9ef140a1
Ice-Jeffrey <735429132@qq.com>
2019-10-15 19:05:17 +0800

    homework submit

--- Homework/Week1/iris.py
+++ Homework/Week1/iris.py
@@ -3,7 +3,8 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from learning_lib import accuracy_score
-
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score as acc
 from bow_classifier import IrisClassifier
 
 
@@ -15,7 +16,7 @@ def load_data():
     return data
 
 
-def main():
+def main1():
     # 1. Load and transform
     iris = datasets.load_iris()
     X = iris.data
@@ -30,8 +31,19 @@ def main():
     predictions = {}
     for (i, message) in enumerate(X_test):
         predictions[i] = model.predict(message)
-    print(accuracy_score(y_test, predictions))
+    print("The accuracy using previous model is: ", accuracy_score(y_test, predictions))
+
+def main2():
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
+    model = MultinomialNB()
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    print("The accuracy using sklearn model is: ", acc(predictions, y_test))
     
 
 if __name__ == "__main__":
-    main()
+    main1()
+    main2()
